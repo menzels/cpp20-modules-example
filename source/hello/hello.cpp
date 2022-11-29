cb5862836eb48cbd3527ace486879e5b1329e1ab
Stefan Menzel <stefan.menzel@mvtec.com>
2022-11-29 16:02:46 +0100

    basic-modules-example-msvc

---
+++ source/hello/hello.cpp
@@ -0,0 +1,5 @@
+export module hello;
+
+#include "hello_export.h"
+
+export HELLO_EXPORT const char *get_hello() { return "Hello World"; }
---
+++ source/main.cpp
@@ -0,0 +1,8 @@
+import hello;
+
+import std.core;
+
+int main() {
+  std::cout << std::format("{0}!\n", get_hello());
+  return 0;
+}
